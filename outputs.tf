39da04d4dce6462d4c8e8f71f2a2e50df9f8387a
Peter Phan <pphan@hashicorp.com>
2021-08-10 15:27:18 -0700

    update outputs

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,12 @@
+output "HCP_CONSUL_ADDR" {
+    value = hcp_consul_cluster.example_hcp.consul_private_endpoint_url
+}
+
 output "VAULT_ADDR" {
     value = hcp_vault_cluster.example_vault_cluster.vault_private_endpoint_url
 }
 
-output "PUBLIC_IP" { 
-    value = "${aws_instance.test-instance.public_ip}" 
+output "PUBLIC_IP" {
+    value = "${aws_instance.test-instance.public_ip}"
 }
 
